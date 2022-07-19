25b046277e812da26c9d4cf4bf089040384e1f93
Florent Morin <florentmorin@icloud.com>
2022-07-19 16:11:49 +0200

    Append app with internal framework

---
+++ AppWithInternalFramework/InternalFramework/InternalFramework/InternalFramework.h
@@ -0,0 +1,18 @@
+//
+//  InternalFramework.h
+//  InternalFramework
+//
+//  Created by Florent Morin on 19/07/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InternalFramework.
+FOUNDATION_EXPORT double InternalFrameworkVersionNumber;
+
+//! Project version string for InternalFramework.
+FOUNDATION_EXPORT const unsigned char InternalFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InternalFramework/PublicHeader.h>
+
+
